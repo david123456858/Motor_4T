66f951f5ce087084c80e43c4b7eed63347c71ace
david123456858 <juandavidperaltafuentes@gmail.com>
2023-10-25 17:46:05 -0500

    circulos de valvulas hechos

--- mainleo.py
+++ mainleo.py
@@ -14,6 +14,8 @@ pygame.display.set_caption("Motor de 4 tiempos")
 # Antes del bucle principal
 piston_background = pygame.image.load("pis.png")
 valvula_img = pygame.image.load("valvula.png")
+caja_image = pygame.image.load('cuerpo.png')
+caja_image= pygame.transform.scale(caja_image, (600, 700))
 # Colores personalizados
 background_color = (255, 255, 255)
 cylinder_color = (0, 0, 0)
@@ -30,7 +32,7 @@ stroke = 0
 
 # Ángulos para el cigüeñal
 crankshaft_angle = 0
-piston_speed = 1  # Velocidad de movimiento del pistón más lenta
+piston_speed = 0.4  # Velocidad de movimiento del pistón más lenta
 crankshaft_length = 100
 piston_length = 100
 
@@ -45,12 +47,17 @@ EXHAUST = 4
 current_state = ADMISSION
 
 # Duración de cada tiempo en frames
-state_duration = 400  # Ahora más lento
+state_duration = 572   # Ahora más lento
 frame_count = 0
 
 # Estado de las válvulas
 valve_admission_open = True
 valve_exhaust_open = False
+def draw_caja():
+    # Dibuja un pistón en la posición y
+    # Dibuja la biela conectada al pistón
+    screen.blit(caja_image, (80, 50))  # Cilindro
+    pygame.draw.rect(screen, background_color, (365, 205, 70, 340))
 
 def draw_piston_and_connecting_rod(piston_x, piston_y, circle_center_x, circle_center_y):
     # Dibuja un pistón en la posición (x, y)
@@ -61,7 +68,7 @@ def draw_piston_and_connecting_rod(piston_x, piston_y, circle_center_x, circle_c
     # pygame.draw.rect(screen, background_color, (piston_x + 10, piston_y + 10, 20, 10))
 
     # Dibuja la biela conectada al pistón
-    piston_center = (piston_x + 20, piston_y + 75)
+    piston_center = (piston_x + 20, piston_y + 70)
     pygame.draw.line(screen, connecting_rod_color, piston_center, (circle_center_x, circle_center_y), 10)
     pygame.draw.rect(screen, background_color, (370, piston_y, 60, 90))
     screen.blit(piston_background2, (350, piston_y))
@@ -95,9 +102,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    
     screen.fill(background_color)
-
+    #draw_caja()
     # Actualiza el ángulo del cigüeñal
     crankshaft_angle += math.radians(piston_speed)
     if crankshaft_angle >= 2 * math.pi:
@@ -105,7 +112,7 @@ while running:
 
     # Calcula la posición del pistón (movimiento vertical)
     piston_position_x = 380
-    piston_position_y = 300 + 100 * math.sin(crankshaft_angle)  # Movimiento vertical
+    piston_position_y = 330 + 70 * math.sin(crankshaft_angle)  # Movimiento vertical
 
     # Calcula la posición del círculo de la biela
     circle_center_x = 400 + 50 * math.cos(crankshaft_angle)
@@ -113,29 +120,29 @@ while running:
 
     # Dibuja las válvulas de admisión y escape
     draw_valves()
-
+    #draw_caja()
     # Actualizar el estado del motor
-    frame_count += 1
+    frame_count += 1.0
     if frame_count > state_duration:
         frame_count = 0
+        valve_exhaust_open = False
         if current_state == ADMISSION:
             current_state = COMPRESSION
             valve_admission_open = False  # Cierra la válvula de admisión
+            
         elif current_state == COMPRESSION:
             current_state = COMBUSTION
         elif current_state == COMBUSTION:
             current_state = EXHAUST
             # Abre la válvula de escape solo cuando el pistón está en su punto más bajo (punto muerto inferior)
-            if piston_position_y >= 250 + piston_length:  # Ajusta el valor apropiado según la posición del pistón
-                valve_exhaust_open = True
-            else:
-                valve_exhaust_open = False
+            valve_admission_open = False  # Abre la válvula de admisión
+            valve_exhaust_open = True  # Cierra la válvula de escape
         else:
             current_state = ADMISSION
-
+    
     # Dibuja el pistón y la biela en los cilindros
     draw_piston_and_connecting_rod(piston_position_x, piston_position_y, circle_center_x, circle_center_y)
-
+    
     text = font.render(f"Estado: {'Admisión' if current_state == ADMISSION else 'Compresión' if current_state == COMPRESSION else 'Combustión' if current_state == COMBUSTION else 'Escape'}", True, (0, 0, 0))
     text_rect = text.get_rect()
     text_rect.center = (screen_width // 2, 20)
@@ -169,6 +176,7 @@ while running:
         pygame.draw.circle(screen, particle_color, (particle_x, particle_y), 2)
     # Dibuja el texto en la ventana
     screen.blit(text, text_rect)
+    
     pygame.display.update()
 
 pygame.quit()
